5a742b7942aebde34953d4f801f776fbbdf09c2e
leslie <panxiang_work@163.com>
2018-08-02 17:47:35 +0800

    河内之塔

---
+++ fibonacci/src/main/java/com/studyinghome/fibonacci/FibonacciApplication.java
@@ -0,0 +1,12 @@
+package com.studyinghome.fibonacci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibonacciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FibonacciApplication.class, args);
+    }
+}
---
+++ towers-of-hanoi/src/test/java/com/studyinghome/towersofhanoi/TowersOfHanoiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.studyinghome.towersofhanoi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TowersOfHanoiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
